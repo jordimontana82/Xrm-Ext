76dc33c488bcecf1077c17b5f26653042b6afa7d
jordimontana82 <jordi.montana@gmail.com>
2014-11-17 02:29:41 +0100

    Initial commit

---
+++ Xrm-Ext.Tests/TestCompare2.cs
@@ -0,0 +1,281 @@
+﻿/*
+Xrm-Ext Unit Tests
+------------------------------------------------
+Copyright (c) 2014 Jordi Montaña, @jordimontana
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+ 
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xrm_Ext.Extensions;
+
+namespace Xrm_Ext.Tests
+{
+    [TestClass]
+    public class TestCompare2
+    {
+        [TestMethod]
+        public void OneNullEntityAndOneNonNullEntityMustBeDifferent()
+        {
+            Entity e1 = null;
+            Entity e2 = new Entity();
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "A null entity must not be equal to a non null entity");
+        }
+
+        [TestMethod]
+        public void CheckNotEqualLogicalNames()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("");
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with different logical names were equal");
+        }
+
+        [TestMethod]
+        public void CheckEqualLogicalNames()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            Assert.IsTrue(e2.IsEqualTo(e1), "Failed: 2 Entities with equal logical names and no attributes weren't equal");
+        }
+
+        [TestMethod]
+        public void CheckDifferentAttributeNumberInThisEntity()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute"] = 1;
+            //e2 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with different number of attributes were equal");
+        }
+
+        [TestMethod]
+        public void CheckDifferentAttributeNumberInOtherEntity()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e2["attribute"] = 1;
+            //e1 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with different number of attributes were equal");
+        }
+
+        [TestMethod]
+        public void CheckSameAttributesWithDifferentNames()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute"] = 1;
+            e2["othername"] = 1;
+            //e1 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with exactly the same number of attributes but different attribute names were equal");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchBool()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = true;
+            e2["attribute1"] = true;
+            //e1 has no attributes
+
+            Assert.IsTrue(e2.IsEqualTo(e1), "Failed: 2 Entities with just one bool attribute with the same name and value for both were different");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchBoolDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = true;
+            e2["attribute1"] = false;
+            //e1 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with just one bool attribute with the same name and different value were equal");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchInteger()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1;
+            e2["attribute1"] = 1;
+            //e1 has no attributes
+
+            Assert.IsTrue(e2.IsEqualTo(e1), "Failed: 2 Entities with just one int attribute with the same name and value for both were different");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchIntegerDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1;
+            e2["attribute1"] = 1234;
+            //e1 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with just one int attribute with the same name and different value for both were equal");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchFloat()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123f;
+            e2["attribute1"] = 1.123f;
+            //e1 has no attributes
+
+            Assert.IsTrue(e2.IsEqualTo(e1), "Failed: 2 Entities with just one float attribute with the same name and value for both were different");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchFloatDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123f;
+            e2["attribute1"] = 1.1234f;
+            //e1 has no attributes
+
+            Assert.IsFalse(e2.IsEqualTo(e1), "Failed: 2 Entities with just one float attribute with the same name and different value for both were equal");
+
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchDouble()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123456;
+            e2["attribute1"] = 1.123456;
+            //e1 has no attributes
+
+            Assert.IsTrue(e1.IsEqualTo(e2), "2 Entities with just one double attribute with the same name and value for both were different");
+        }
+
+        [TestMethod]
+        public void CheckDifferentDoubleAttributeValuesAreDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123456;
+            e2["attribute1"] = 13212.234;
+            //e1 has no attributes
+
+            Assert.IsFalse(e1.IsEqualTo(e2), "2 Entities with just one double attribute with the same name and different value for both were equal");
+        }
+
+        [TestMethod]
+        public void CheckAttributeValueMatchDecimal()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123456m;
+            e2["attribute1"] = 1.123456m;
+            //e1 has no attributes
+
+            Assert.IsTrue(e1.IsEqualTo(e2), "2 Entities with just one decimal attribute with the same name and value for both were different");
+        }
+
+        [TestMethod]
+        public void CheckDifferentDecimalAttributeValuesAreDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1.123456m;
+            e2["attribute1"] = 13212.234m;
+            //e1 has no attributes
+
+            Assert.IsFalse(e1.IsEqualTo(e2), "2 Entities with just one decimal attribute with the same name and different value for both were equal");
+        }
+
+        [TestMethod]
+        public void CheckTwoEntitiesWithOneAttributeWithDifferentDataTypesAreDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1; //int
+            e2["attribute1"] = 1.0f; //float
+            //e1 has no attributes
+
+            Assert.IsFalse(e1.IsEqualTo(e2), "2 Entities with just one int and float attribute with the same value but different types were equal");
+        }
+
+        [TestMethod]
+        public void CheckTwoEntitiesWithTwoAttributesWithSameValuesAreEqual()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1; //int
+            e1["attribute2"] = 20; //int
+
+            e2["attribute1"] = 1; //int
+            e2["attribute2"] = 20; //int
+
+            //e1 has no attributes
+
+            Assert.IsTrue(e1.IsEqualTo(e2), "2 Entities with the 2 same attribute values were different");
+        }
+
+        [TestMethod]
+        public void CheckTwoEntitiesWithTwoAttributesWithOneDifferentValuesAreDifferent()
+        {
+            Entity e1 = new Entity("logicalName1");
+            Entity e2 = new Entity("logicalName1");
+
+            e1["attribute1"] = 1; //int
+            e1["attribute2"] = 3; //int
+
+            e2["attribute1"] = 1; //int
+            e2["attribute2"] = 20; //int
+
+            //e1 has no attributes
+
+            Assert.IsFalse(e1.IsEqualTo(e2), "2 Entities with the 2 attributes, one different, were equal");
+        }
+    }
+}
---
+++ Xrm-Ext/Extensions/Compare2.cs
@@ -0,0 +1,133 @@
+﻿/*The MIT License (MIT)
+
+Copyright (c) 2014 Jordi Montaña, @jordimontana
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+ 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+
+
+namespace Xrm_Ext.Extensions
+{
+    /// <summary>
+    /// This class contains extension methods to compare two Entity records.
+    /// </summary>
+    public static class Compare2
+    {
+        /// <summary>
+        /// Compares this Entity against another instance, comparing every single attribute
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static bool IsEqualTo(this Entity @this, Entity other)
+        {
+            if (other == null) return false; //As @this is always defined
+
+            //@this and other not null, check logicalnames
+            if (!@this.LogicalName.Equals(other.LogicalName))
+                return false;  //Logical names do not match (we were trying to compare records from different entities...
+
+            //Check that all the attributes in @this exist in 'other' with exactly the same values and viceversa
+            foreach (var att in @this.Attributes)
+            {
+                if (other.Attributes.ContainsKey(att.Key))
+                {
+                    if (!AreValuesEqual(att.Value, other[att.Key]))
+                        return false;
+                }
+                else
+                    return false;  //@this has an attribute that other doesn't
+
+            }
+
+            //Same thing the other way around...
+            foreach (var att in other.Attributes)
+            {
+                if (@this.Attributes.ContainsKey(att.Key))
+                {
+                    if (!AreValuesEqual(att.Value, @this[att.Key]))
+                        return false;
+                }
+                else
+                    return false;  //@this has an attribute that other doesn't
+
+            }
+
+            //At this point, all the attributes match!
+            return true;
+        }
+
+        /// <summary>
+        /// Compares two values, and returns true if both had the same value.
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool AreValuesEqual(object v1, object v2)
+        {
+            //If both values are null, then they are actually equal
+            if (v1 == null && v2 == null) return true;
+
+            //If one of the values is null (and the other doesn't, then they are different)
+            if (v1 == null) return false;
+            if (v2 == null) return false;
+
+            //Then, specific comparison depending on the data type:
+
+            switch (v1.GetType().ToString())
+            {
+                case "EntityReference":
+                    if (!(v2 is EntityReference)) return false; //The other value is not an entity reference, so they are indeed different
+
+                    return (v1 as EntityReference).Id == (v2 as EntityReference).Id;
+
+                case "System.Bool":
+                    if (!(v2 is bool)) return false; //The other value is not a bool type, so they are different
+
+                    return (bool)v1 == (bool)v2;
+
+                case "OptionSetValue":
+                    if (!(v2 is OptionSetValue)) return false;
+
+                    return (v1 as OptionSetValue).Value == (v2 as OptionSetValue).Value; //value changed if the int values are different
+
+                case "System.Single":
+                    if (!(v2 is float)) return false; //The other value is not a float type, so they are different
+
+                    return (float)v1 == (float)v2;
+
+                case "System.Double":
+                    if (!(v2 is double)) return false; //The other value is not a float type, so they are different
+
+                    return (double)v1 == (double)v2;
+                default:
+                    //Comparison using strings
+                    return string.Equals(v1.ToString(), v2.ToString()); //value changed if the values are different
+
+            }
+        }
+    }
+}
