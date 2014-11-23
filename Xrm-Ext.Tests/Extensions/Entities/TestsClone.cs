38beff2188923794377b7bf2368e059eea2e8f7d
jordimontana82 <jordi.montana@gmail.com>
2014-11-23 03:14:29 +0100

    Added Clone extension method + tests

---
+++ Xrm-Ext.Tests/Extensions/Entities/TestsClone.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+using Xrm_Ext.Extensions.Entities;
+
+namespace Xrm_Ext.Tests.Extensions.Entities
+{
+    public class TestsClone
+    {
+        [Fact]
+        public void A_cloned_entity_with_one_attribute_should_be_equal_to_the_original()
+        {
+            var originalEntity = new Entity("account");
+            originalEntity["name"] = "Super account";
+
+            var cloned = originalEntity.Clone();
+            Assert.True(originalEntity.IsEqualTo(cloned));
+        }
+
+        [Fact]
+        public void A_cloned_entity_does_not_contain_the_primary_attribute()
+        {
+            var originalEntity = new Entity("account");
+            originalEntity["name"] = "Super account";
+            originalEntity["accountid"] = Guid.NewGuid();
+
+            var cloned = originalEntity.Clone();
+
+            Assert.False(cloned.Attributes.ContainsKey("accountid"));
+        }
+    }
+}
